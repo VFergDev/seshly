12df025596ef1245f32af299cbe26cf8b915cb6f
Viana Ferguson <vianaferguson@yahoo.com>
2024-01-04 23:25:25 -0700

    commit all

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,20 +1,27 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGO_URL;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
-    bufferCommands: false,
-  })
+  try {
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+      dbName: 'evently',
+      bufferCommands: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as mongoose.ConnectOptions);
 
-  cached.conn = await cached.promise;
+    cached.conn = await cached.promise;
 
-  return cached.conn;
+    return cached.conn;
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error;
+  }
 }
